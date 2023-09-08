47267660101b8aadfb7b58942da07023ee132920
bccarruth927 <brecarr1997@aol.com>
2023-09-08 13:06:13 -0400

    Remove the auto play functionality from nav

--- js/services.swiper.js
+++ js/services.swiper.js
@@ -7,13 +7,13 @@ $(document).ready(() => {
         centeredSlides: true,
         grabCursor: true,
         loop: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
     });
 });
